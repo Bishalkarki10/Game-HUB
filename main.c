455fc202c36c46dd25dd7abe6af82c0e5d8ca57b
Bishal Karki <111341977+Bishalkarki10@users.noreply.github.com>
2023-01-15 21:16:38 +0545

    first commit

---
+++ main.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include "menu.h"
+#include<windows.h>
+
+// top margin...
+void topspace(){
+	printf("\n\n\n");
+    printf("\n\n\n");
+    printf("\n\n\n");
+    printf("\n\n\n");
+	}
+	
+int main(){
+	system("color F");
+	topspace();
+
+    printf("\033[1;37m"); // change text size to bigger
+	printf("\t\t\t     &&&&&&&&&&&&&\n");
+	printf("\t\t\t     &&           \n");
+	printf("\t\t\t     &&   &&&&&&&&\n");
+	printf("\t\t\t     &&   &&&&&  &\n");
+	printf("\t\t\t     &&      &&  &\n");
+	printf("\t\t\t     &&      &&  &\n");
+	printf("\t\t\t     &&&&&&&&&&  &\n");
+	Sleep(700);
+	system("cls");
+	
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&      \n");
+	printf("\t\t\t     &&                  &&   &&     \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&    \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&   \n");
+	printf("\t\t\t     &&      &&  &    &&         &&  \n");
+	printf("\t\t\t     &&      &&  &   &&           && \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&\n");
+	Sleep(700);
+	system("cls");
+	
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&& \n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  && \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  && \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   && \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          && \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          && \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          && \n");
+	Sleep(700);   
+	system("cls");
+	
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&&  &&&&&&&&&&\n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   &&  &&&&&&&&& \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          &&  &&        \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          &&  &&        \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          &&  &&&&&&&&&&\n");
+	Sleep(700);
+	printf("\n\n");
+	printf("\n\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&&&&&&&&&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+    
+    Sleep(700);
+    system("cls");
+    topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&&  &&&&&&&&&&\n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   &&  &&&&&&&&& \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          &&  &&        \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          &&  &&        \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          &&  &&&&&&&&&&\n");
+	printf("\n\n");
+	printf("\n\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&&&&&&&&&   &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&&&&&&&&&\n");
+    
+    Sleep(700);
+    system("cls");
+    topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&&  &&&&&&&&&&\n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   &&  &&&&&&&&& \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          &&  &&        \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          &&  &&        \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          &&  &&&&&&&&&&\n");
+	printf("\n\n");
+	printf("\n\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&&&&&&&&& \n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+    printf("\t\t\t\t\t\t\t\t\t         &&&&&&&&&&   &&      &&   &&&&&&&&&  \n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+    printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&&&&&&&&&   &&&&&&&&&& \n");
+    Sleep(1500);
+    system("cls");
+	printf("\033[0m"); // reset text format
+    //calling function menu from menu.h header file
+    menu();
+	return 0;
+}
